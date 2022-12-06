71115b45710184443d2fb6dfd8057b887ca1c66f
mustafademir06 <mustafade@gmail.com>
2022-12-06 19:31:56 +0300

    20021206

--- src/test/java/pages/Page_07.java
+++ src/test/java/pages/Page_07.java
@@ -1,5 +1,112 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class Page_07 {
+    public Page_07() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy (xpath = "//a[@class='appointment-btn scrollto']") public WebElement makeAnAppointmentButton;
+    @FindBy (xpath = "//input[@id='firstName']") public WebElement firstName;
+    @FindBy (xpath = "//input[@id='lastName']") public WebElement lastName;
+    @FindBy (xpath = "//input[@id='ssn']") public WebElement ssn;
+    @FindBy (xpath = "//input[@id='email']") public WebElement email;
+    @FindBy (xpath = "//input[@id='phone']") public WebElement phone;
+    @FindBy (xpath = "//input[@id='appoDate']") public WebElement appoDate;
+    @FindBy (xpath = "//span[text()='Send an Appointment Request']") public WebElement sendAndAppointmentRequestButton;
+    @FindBy (xpath = "/*[contains(text(),'Appointment registration saved!')]") public WebElement randevuKaydedildiYazisi;
+
+    @FindBy (xpath = "//div[@role='alert']") public WebElement randevuKaydedildiYazisiSHub;
+
+
+    @FindBy (xpath = "//div[text()='Your FirstName is required.']") public WebElement firstnameRequiredText;
+    @FindBy (xpath = "//div[text()='Phone number is invalid']") public WebElement phoneNumberInvalidText;
+    @FindBy (xpath = "//div[text()='Your SSN is required.']") public WebElement ssnRequiredText;
+    @FindBy (xpath = "//div[text()='Your LastName is required.']") public WebElement lastnameRequiredText;
+    @FindBy (xpath = "//div[text()='Phone number is required.']") public WebElement phonenumberRequiredText;
+    @FindBy (xpath = "//li[@id='account-menu']") public WebElement accountMenu;
+    @FindBy (xpath = "//span[text()='Sign in']") public WebElement signInButton;
+    @FindBy (xpath = "//input[@id='username']") public WebElement username;
+    @FindBy (xpath = "//input[@id='password']") public WebElement password;
+    @FindBy (xpath = "(//span[text()='Sign in'])[3]") public WebElement signIn2Button;
+    @FindBy (xpath = "//span[text()='MY PAGES(PATIENT)']") public WebElement myPageSheet;
+    @FindBy (xpath = "//span[text()='My Appointments']") public WebElement myAppointmentButton;
+    @FindBy (xpath = "//h2[@id='appointment-heading']") public WebElement appointmentBasligi;
+
+
+  //  @FindBy (xpath = "(//span[text()='Sign in'])[3]") public WebElement signIn2Button;
+
+    @FindBy (xpath = "(//tr)[2]") public List<WebElement> appointmentFirstRowWE;
+    @FindBy (css = "#newPassword.is-touched") public WebElement PhoneBoxBlueFrame;
+    @FindBy (xpath = "//tbody//tr//td[1]") public List<WebElement> idSutunElemanlariWE;
+    @FindBy (xpath = "//span[text()='Make an Appointment']") public WebElement makeAnAppointment2;
+    @FindBy (xpath = "//span[text()='CONTACT']") public WebElement contactMenu;
+    @FindBy (xpath = "//input[@id='name']") public WebElement fullNameBox;
+    @FindBy (xpath = "//input[@id='email']") public WebElement emailBox;
+    @FindBy (xpath = "//input[@id='subject']") public WebElement subjectBox;
+    @FindBy (xpath = "//textarea[@id='message']") public WebElement messageBox;
+    @FindBy (xpath = "//span[text()='Send']") public WebElement sendButton;
+    @FindBy (xpath = "//section[@id='contact']") public WebElement contactSayfasi;
+    @FindBy (xpath = "//*[text()='Your message has been received']") public WebElement mesajIletildiYazisi;
+    @FindBy (xpath = "//*[contains(text(),'Your message has been received')]") public WebElement mesajIletildiYazisiContains;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static String tarih_Uret(int gun) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, gun);
+        String date = sdf.format(calendar.getTime());
+        return date;
+    }
+
+
+
 
 }
--- src/test/java/stepDefinitions/apiSteps/US027.java
+++ src/test/java/stepDefinitions/apiSteps/US027.java
@@ -1,4 +1,93 @@
 package stepDefinitions.apiSteps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import pojos.ContactPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static utilities.Authentication.generateToken;
+
 public class US027 {
+
+    Response responsePost;
+    Response responseGet;
+    ContactPojo expectedDataPost;
+    ContactPojo actualDataPost;
+    ContactPojo expectedDataGet;
+    ContactPojo actualDataGet;
+
+    @Given("md Kullanici token alip ileti olusturmak icin POST REQUEST yapar ve response alir")
+    public void md_kullanici_token_alip_ileti_olusturmak_icin_post_request_yapar_ve_response_alir() {
+
+        /*"email": "deneme2@deneme.com",
+        "message": "TestNG daha stabil calisiyor2",
+        "name": "testNG22",
+        "subject": "Hangi framework2"
+         */
+        expectedDataPost = new ContactPojo("deneme2@deneme.com","TestNG daha stabil calisiyor2","testNG22","Hangi framework2");
+        System.out.println("expectedData = " + expectedDataPost);
+
+        String url = "https://medunna.com/api/c-messages";
+        responsePost = given().headers("Authorization", "Bearer " + generateToken()).
+                contentType(ContentType.JSON).body(expectedDataPost).when().post(url);
+        responsePost.prettyPrint();
+
+
+    }
+    @Then("md Respons'larin dogrulamasini yapar")
+    public void md_respons_larin_dogrulamasini_yapar() {
+
+        actualDataPost = responsePost.as(ContactPojo.class);
+        System.out.println("actualData = " + actualDataPost);
+        assertEquals(201, responsePost.getStatusCode());
+        assertEquals(expectedDataPost.getEmail(),actualDataPost.getEmail());
+        assertEquals(expectedDataPost.getMessage(),actualDataPost.getMessage());
+        assertEquals(expectedDataPost.getName(),actualDataPost.getName());
+        assertEquals(expectedDataPost.getSubject(),actualDataPost.getSubject());
+
+    }
+
+    @Given("md Kullanici token alip ileti getirmek icin GET REQUEST yapar ve response alir\"")
+    public void md_kullanici_token_alip_ileti_getirmek_icin_get_request_yapar_ve_response_alir() {
+        /*
+        {"id": 364775,
+        "name": "testNG22",
+        "email": "deneme2@deneme.com",
+        "subject": "Hangi framework222",
+        "message": "TestNG daha stabil calisiyor222"
+
+        {
+    "id": 364775,
+    "name": "testNG22",
+    "email": "deneme2@deneme.com",
+    "subject": "Hangi framework222",
+    "message": "TestNG daha stabil calisiyor222"
+}
+
+}     */
+        expectedDataGet = new ContactPojo(364775,"testNG22","deneme2@deneme.com","Hangi framework222","TestNG daha stabil calisiyor222");
+        System.out.println("expecteddataGet = " + expectedDataGet);
+
+        String url = "https://medunna.com/api/c-messages/364775";
+        responseGet = given().headers("Authorization", "Bearer " + generateToken()).when().get(url);
+        responseGet.prettyPrint();
+
+    }
+    @Then("md Respons'larin dogrulamasini yapar\\(getRequest)")
+    public void md_respons_larin_dogrulamasini_yapar_get_request() {
+        actualDataGet = responseGet.as(ContactPojo.class);
+        System.out.println("actualData = " + actualDataGet);
+        assertEquals(200,responseGet.getStatusCode());
+        assertEquals(expectedDataGet.getId(), actualDataGet.getId());
+        assertEquals(expectedDataGet.getName(), actualDataGet.getName());
+        assertEquals(expectedDataGet.getEmail(), actualDataGet.getEmail());
+        assertEquals(expectedDataGet.getSubject(), actualDataGet.getSubject());
+        assertEquals(expectedDataGet.getMessage(), actualDataGet.getMessage());
+
+
+    }
+
 }
