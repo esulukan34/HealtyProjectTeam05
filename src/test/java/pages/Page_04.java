cb7ea7925f3976b6c2db2e1b956a47e1003cd905
Ahmet İnan <ahmetinanyunefa@gmail.com>
2022-12-10 17:06:08 +0300

    10.12

--- src/test/java/pages/Page_04.java
+++ src/test/java/pages/Page_04.java
@@ -1,88 +1,46 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class Page_04 {
 
+public class Page_04 {
     public Page_04(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-
-    @FindBy(css =".d-flex.align-items-center.dropdown-toggle")
-    public WebElement account;
-
-    @FindBy(xpath = "//*[@class='dropdown-item'][2]")
-    public WebElement register;
-
-    @FindBy(css = "#ssn")
-    public WebElement ssn;
-
+    @FindBy (xpath="//*[@id='login-item']")
+    public WebElement sign;
+    @FindBy(xpath = "//*[@class='d-flex align-items-center dropdown-toggle nav-link']")
+    public WebElement accountmenu;
     @FindBy(css = "#firstName")
     public WebElement firstname;
-
+    @FindBy(xpath = "//input[@name='username']")
+    public WebElement username;
     @FindBy(css = "#lastName")
     public WebElement lastname;
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement pasword;
+    @FindBy(xpath = "//*[@class='btn btn-primary']")
+    public WebElement signButon;
+    @FindBy (xpath = "//*[@src='content/images/logo.png']")
+    public WebElement Medunna;
 
-    @FindBy(css = ".invalid-feedback")
-    public WebElement gecersizSsn;
-
-    @FindBy(css = "#register-submit")
-    public WebElement gecersizRegister;
-
-    @FindBy(xpath = "//*[text()='Your SSN is required.']")
-    public WebElement requiredSsn;
 
     @FindBy(xpath = "//*[text() = 'Your FirstName is required.']")
-    public WebElement requiredName;
-
-    @FindBy(xpath = "//*[text()='Your LastName is required.']")
-    public WebElement requiredLastname;
-
-    @FindBy(css = "#username")
-    public WebElement username;
-
-    @FindBy(css = "#email")
-    public WebElement email;
-
-    @FindBy(css = "#firstPassword")
-    public WebElement password;
-
-    @FindBy(css = "#secondPassword")
-    public WebElement password2;
-
-    @FindBy(xpath = "//*[text()='Register'][1]")
-    public WebElement register2;
-
-    @FindBy(xpath = "//*[text()='MY PAGES(PATIENT)']")
-    public WebElement myPages;
-
-    @FindBy(xpath = "//*[text()='Make an Appointment'][1]")
-    public WebElement mAppointment;
-
-    @FindBy(css = "#phone")
-    public WebElement phone;
-
-    @FindBy(css = "#appoDate")
-    public WebElement date;
-
-    @FindBy(css = "#register-submit")
-    public WebElement sendAppointment;
-    //365175
-
-    @FindBy(css =".Toastify__toast-body")
-    public WebElement randevuOnayi;
-
-    @FindBy(xpath = "//*[text()='Sign in']")
-    public WebElement signIn;
-
-    @FindBy(css = "#username")
-    public WebElement patientUsername;
-
-    @FindBy(css = "#password")
-    public WebElement patientPassword;
-
-
+    public WebElement gecersizName;
+    @FindBy (xpath = "//label[@class='form-check-label']")
+    public WebElement Remem_me;
+    @FindBy (xpath = "//div[text()='Authentication information not correct.']")
+    public WebElement Başarısızmesajı;
+    @FindBy (xpath = "//div[contains(text(),'Authentication information not correct.')]")
+    public WebElement bsarımesajımı_acaba;
+    @FindBy (xpath = "//*[@href=\"/account/reset/request\"]")
+    public WebElement ŞifremiUnuttum;
+    @FindBy (xpath = "//*[text()='Register a new account']")
+    public WebElement Register_a_new_account;
+    @FindBy (xpath = "//*[text()='Cancel']")
+    public WebElement cancel;
 }
