67f80b83b2ffc9c1c7bd8e7df908b3dace12653b
Emre Sulukan <esulukan34@gmail.com>
2022-12-12 20:51:23 +0300

    12 Aralık

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@gp",       //2 tagi ayni anada calistirmak isterseniz "@gp1 or @gp2" seklinde yazariz
+        tags = "@US25NEG2",       //2 tagi ayni anada calistirmak isterseniz "@gp1 or @gp2" seklinde yazariz
         dryRun = false
 )
 
